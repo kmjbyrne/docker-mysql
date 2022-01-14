cee6c319d9acd639f9f09a995badcd9e74d48646
Keith Byrne <keithmbyrne@gmail.com>
2022-01-14 14:27:40 +0000

    FEAT: add SQL bootstrap script

---
+++ scripts/setup.sql
@@ -0,0 +1,6 @@
+-- create the initial username and password user account
+CREATE USER '{{USERNAME}}' @'%' IDENTIFIED BY '{{PASSWORD}}';
+GRANT ALL PRIVILEGES ON *.* TO '{{USERNAME}}' @'%';
+FLUSH PRIVILEGES;
+-- create a schema
+CREATE SCHEMA `{{DB_NAME}}`;
