0ea020b695c4221e694bc60a4b23077999edaa58
Keith Byrne <keithmbyrne@gmail.com>
2022-01-14 14:36:16 +0000

    MINOR: rename scripts for service relevance

--- scripts/setup.sql
+++ scripts/setup.sql
@@ -1,4 +1,5 @@
 -- create the initial username and password user account
+DROP USER '{{USERNAME}}' @'%';
 CREATE USER '{{USERNAME}}' @'%' IDENTIFIED BY '{{PASSWORD}}';
 GRANT ALL PRIVILEGES ON *.* TO '{{USERNAME}}' @'%';
 FLUSH PRIVILEGES;
